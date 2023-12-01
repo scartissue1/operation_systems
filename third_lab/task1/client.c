78e12e3dbcd3f0533cce29e0aa06cbcf85deeb30
Daniil Vasilev <82233682+scartissue1@users.noreply.github.com>
2023-12-01 17:24:23 +0300

    Update client.c

--- third_lab/task1/client.c
+++ third_lab/task1/client.c
@@ -1,34 +1,112 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
-#include "erproc.h"
+typedef enum {
+    OK,
+    KEY_ERROR,
+    QUEUE_INIT_ERROR,
+    INVALID_PARAMETER,
+    NO_FILE
+} status_codes;
 
-int main(int argc, char *argv[]) {
-    int fd = Socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = {0};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(34543);
-    Inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
-    Connect(fd, (struct sockaddr *) &addr, sizeof addr);
-    while (1) {
-        printf("Input the file path: (Or Q if you want to close the connection)\n");
-        char buf[128];
-        scanf("%s", buf);
-
-        if (!strcmp(buf, "Q")) {
-            break;
-        }
-
-        write(fd, buf, 128);
-        read(fd, buf, 127);
-        printf("%s\n", buf);
+void errorProcessing(const status_codes error) {
+    switch (error) {
+        case KEY_ERROR:
+            printf("Wrong server/client key detected\n");
+            return;
+        case QUEUE_INIT_ERROR:
+            printf("Unable to open message queue\n");
+            return;
+        case INVALID_PARAMETER:
+            printf("Invalid message detected\n");
+            return;
+        case NO_FILE:
+            printf("No such file\n");
+            return;
     }
-    close(fd);
+}
+
+struct msg_buffer {
+    long msg_type;
+    char msg_text[BUFSIZ];
+};
+
+const int PRIOR_MAX = 100;
+
+int main() {
+
+    key_t client_key = ftok("client.txt", 1);
+    key_t server_key = ftok("server.txt", 1);
+    
+    if (client_key == -1 || server_key == -1) {
+        errorProcessing(KEY_ERROR);
+        return -1;
+    }
+    
+    int client_queue = msgget(client_key, 0666 | IPC_CREAT);
+    int server_queue = msgget(server_key, 0666 | IPC_CREAT);
+
+    if (client_queue == -1 || server_queue == -1) {
+        errorProcessing(QUEUE_INIT_ERROR);
+        msgctl(server_queue, IPC_RMID, NULL);
+        msgctl(client_queue, IPC_RMID, NULL);
+        return -1;
+    }
+    struct msg_buffer mesg;
+
+    FILE *stream = fopen("message.txt", "r");
+    if (!stream) {
+        errorProcessing(NO_FILE);
+        msgctl(server_queue, IPC_RMID, NULL);
+        msgctl(client_queue, IPC_RMID, NULL);
+        return -1;
+    }
+    
+    int count = 0;
+    char buf[BUFSIZ];
+    while (fgets(buf, sizeof(char) * BUFSIZ, stream) != NULL) {
+        count++;
+        strtok(buf, "=");
+        char *string_priority = strtok(NULL, " ");
+        if (!string_priority) {
+            errorProcessing(NO_FILE);
+            msgctl(server_queue, IPC_RMID, NULL);
+            msgctl(client_queue, IPC_RMID, NULL);
+            return -1;
+        }
+        int priority = atoi(string_priority);
+        priority = (priority < 0) ? 0 : priority;
+        priority = (priority > PRIOR_MAX - 1) ? PRIOR_MAX - 1 : priority;
+        mesg.msg_type = priority;
+        printf("%ld\n", mesg.msg_type);
+        strtok(NULL, "\"");
+        char *message = strtok(NULL, "\n");
+        if (!message) {
+            errorProcessing(INVALID_PARAMETER);
+            msgctl(server_queue, IPC_RMID, NULL);
+            msgctl(client_queue, IPC_RMID, NULL);
+            fclose(stream);
+            return -1;
+        }
+        printf("%s\n", message);
+        size_t message_size = strlen(message);
+        for (int i = 0; i < message_size - 2; i++) {
+            mesg.msg_text[i] = message[i];
+        }
+        msgsnd(server_queue, &mesg, sizeof(mesg), 0);
+    }
+    for (int i = 0; i < count; i++) {
+        msgrcv(client_queue, &mesg, sizeof(mesg), -PRIOR_MAX, IPC_NOWAIT);
+        printf("%s\n", mesg.msg_text);
+    }
+    mesg.msg_type = PRIOR_MAX;
+    msgsnd(server_queue, &mesg, sizeof(mesg), 0);
+    fclose(stream);
+    msgctl(server_queue, IPC_RMID, NULL);
+    msgctl(client_queue, IPC_RMID, NULL);
     return 0;
 }
